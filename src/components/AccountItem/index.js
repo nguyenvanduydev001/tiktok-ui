c7560f02a72a479a070ec9eeead17b87e74c7064
nguyenvanduydev001 <nguyenvanduydev001@gmail.com>
2024-03-13 09:51:09 +0700

    Xây dựng Logic phần Header #2

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,27 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/da02bb136deaab5f2b9952eae0ccf335~c5_100x100.jpeg?lk3s=a5d48078&x-expires=1709812800&x-signature=OcozxObbceqlKFFpJKoN2UMNP%2FU%3D"
-                alt="Duy"
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Nguyen Van Duy</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>nguyenvanduy</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '~/App'; // Kiểm tra đường dẫn đến App.js
+import App from '~/App';
 import reportWebVitals from './reportWebVitals';
-import './components/GlobalStyles/GlobalStyles.scss'; // Import tệp SCSS ở đây
+import GlobalStyles from '~/components/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
     </React.StrictMode>,
 );
 
-// Nếu bạn muốn đo lường hiệu suất trong ứng dụng của bạn, bạn có thể sử dụng reportWebVitals như sau:
 reportWebVitals();
